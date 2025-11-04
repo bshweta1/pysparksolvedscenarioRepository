cb3eb5ab43ad2a6387745969f9598518a1cf9fb4
bshweta1 <bshweta3110@gmail.com>
2025-11-04 23:20:08 +0530

    Add files via upload

---
+++ Des_manager.py
@@ -0,0 +1,76 @@
+# For Employee salary greater than 10000 give designation as manager else employee
+import os
+import urllib.request
+import ssl
+
+data_dir = "data"
+os.makedirs(data_dir, exist_ok=True)
+
+data_dir1 = "hadoop/bin"
+os.makedirs(data_dir1, exist_ok=True)
+
+hadoop_home = os.path.abspath("hadoop")   # <-- absolute path
+os.makedirs(os.path.join(hadoop_home, "bin"), exist_ok=True)
+
+urls_and_paths = {
+    "https://raw.githubusercontent.com/saiadityaus1/SparkCore1/master/test.txt":os.path.join(data_dir, "test.txt"),
+    "https://github.com/saiadityaus1/SparkCore1/raw/master/winutils.exe":os.path.join(hadoop_home, "bin", "winutils.exe"),
+    "https://github.com/saiadityaus1/SparkCore1/raw/master/hadoop.dll":os.path.join(hadoop_home, "bin", "hadoop.dll")
+}
+
+# Create an unverified SSL context
+
+# ======================================================================================
+
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+import sys
+import os
+import urllib.request
+import ssl
+
+python_path = sys.executable
+os.environ['PYSPARK_PYTHON'] = python_path
+os.environ['HADOOP_HOME'] = hadoop_home
+os.environ['JAVA_HOME'] = r'C:\Users\DELL\.jdks\corretto-1.8.0_462'        #  <----- 🔴JAVA PATH🔴
+######################🔴🔴🔴################################
+
+#os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages com.datastax.spark:spark-cassandra-connector_2.12:3.5.1 pyspark-shell'
+#os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-avro_2.12:3.5.4 pyspark-shell'
+#os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.4 pyspark-shell'
+
+
+conf = SparkConf().setAppName("pyspark").setMaster("local[*]").set("spark.driver.host","localhost").set("spark.default.parallelism", "1")
+sc = SparkContext(conf=conf)
+
+spark = SparkSession.builder.getOrCreate()
+
+#spark.read.format("csv").load("data/test.txt").toDF("Success").show(20, False)
+
+##################🔴🔴🔴🔴🔴🔴 -> DONT TOUCH ABOVE CODE -- TYPE BELOW ####################################
+
+data = [
+    ("1", "a", 10000),
+    ("2", "b", 5000),
+    ("3", "c", 15000),
+    ("4", "d", 25000),
+    ("5", "e", 50000),
+    ("6", "f", 7000),
+]
+
+schema=["id","emp","salary"]
+
+df=spark.createDataFrame(data,schema=schema)
+
+df.createOrReplaceTempView("df")
+
+result = spark.sql("""
+    SELECT id,
+           emp,
+           salary,
+           CASE WHEN salary >10000 THEN 'Manager'
+                ELSE 'Employee'
+           END AS designation
+    FROM df
+""").show()
